bca48aabcb475bcb5989de3413f59555212c2778
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-24 02:01:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,10 +27,9 @@ class Scraper
 
   def make_courses
     get_courses.each do |course|
-      binding.pry
       new_course = Course.new
       new_course.title = course.css('h2').text
-      new_course.schedule = course.css('em').text
+      new_course.schedule = course.css('.date').text
       new_course.description = course.css('p').text
     end
   end
